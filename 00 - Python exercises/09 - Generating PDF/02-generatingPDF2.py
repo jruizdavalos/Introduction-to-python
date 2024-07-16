01265627814aa282e2684c880b9f7e9f2868e517
Juan Ruiz <jaruizdavalos@gmail.com>
2024-07-15 22:42:30 -0300

    creating pdf part 2

--- 00 - Python exercises/09 - Generating PDF/02-generatingPDF2.py
+++ 00 - Python exercises/09 - Generating PDF/02-generatingPDF2.py
@@ -3,13 +3,14 @@ from fpdf import FPDF
 class PDF(FPDF):
   def header(self):
     self.image("00 - Python exercises/09 - Generating PDF/logo.png", 10,8,33)
+    self.set_font("helvetica","B",16)
+    self.cell(80)
+    self.cell(40,10,"Hello world",border=2, align="C")
 
 
 pdf= PDF()
 
 pdf.add_page()
-pdf.set_font("helvetica","B",16)
 
-pdf.cell(40,10,"Hello world")
 
-pdf.output("00 - Python exercises/09 - Generating PDF/sample2.pdf")
+pdf.output("00 - Python exercises/09 - Generating PDF/sample3.pdf")
