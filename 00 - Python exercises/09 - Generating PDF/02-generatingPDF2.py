57d207005988177ebbd9512a6a9ae0302076a3b2
Juan Ruiz <jaruizdavalos@gmail.com>
2024-07-16 22:33:36 -0300

    generation pdf part 3

--- 00 - Python exercises/09 - Generating PDF/02-generatingPDF2.py
+++ 00 - Python exercises/09 - Generating PDF/02-generatingPDF2.py
@@ -5,12 +5,20 @@ class PDF(FPDF):
     self.image("00 - Python exercises/09 - Generating PDF/logo.png", 10,8,33)
     self.set_font("helvetica","B",16)
     self.cell(80)
-    self.cell(40,10,"Hello world",border=2, align="C")
+    self.cell(40,10,"hello world",border=1, align="C")
+    self.ln(40)
 
+  def footer(self):
+    self.set_y(-15)
+    self.set_font("helvetica","I",8)
+    self.cell(0,10, f"Page {self.page_no()}/{{nb}}",align="C")
 
 pdf= PDF()
 
 pdf.add_page()
+pdf.set_font("helvetica","B",16)
+for i in range(1,41):
+  pdf.cell(0,10, f"Printing line number {i}", new_x="LMARGIN", new_y="NEXT")
 
 
 pdf.output("00 - Python exercises/09 - Generating PDF/sample3.pdf")
