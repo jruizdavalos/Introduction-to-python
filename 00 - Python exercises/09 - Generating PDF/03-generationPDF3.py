4af22654cd97cd168448a9bddaed0e6ff2c7e730
Juan Ruiz <jaruizdavalos@gmail.com>
2024-07-18 23:48:26 -0300

    Fail with reading paragraph.txt

--- 00 - Python exercises/09 - Generating PDF/03-generationPDF3.py
+++ 00 - Python exercises/09 - Generating PDF/03-generationPDF3.py
@@ -1,7 +1,7 @@
 from fpdf import FPDF
 
 
-class PDF():
+class PDF(FPDF):
   def header(self):
     pass
   
@@ -12,10 +12,14 @@ class PDF():
     pass
 
   def chapter_body(self, filepath):
+
     with open(filepath,"rb") as fh:
       txt = fh.read().decode('latin-1')
-
-    print(txt)
+    self.set_font("Times", size=12)
+    self.multi_cell(0,5,txt)
+    self.ln()
+    self.set_font(style="I")
+    self.cell(0, 5, "(End of excernt)")
     pass
 
   def print_chapter(self, filepath):
@@ -23,5 +27,6 @@ class PDF():
     pass
 
 pdf = PDF()
-
-pdf.print_chapter("paragraph.txt")
+txt="00 - Python exercises/09 - Generating PDF/paragraph.txt"
+pdf.print_chapter(txt)
+pdf.output("sample4.pdf")
