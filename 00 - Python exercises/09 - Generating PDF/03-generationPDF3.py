f9eb7ca87b6117486d690f34e6fc8d4b60540513
Juan Ruiz <jaruizdavalos@gmail.com>
2024-07-21 21:12:38 -0300

    Creating PDF

--- 00 - Python exercises/09 - Generating PDF/03-generationPDF3.py
+++ 00 - Python exercises/09 - Generating PDF/03-generationPDF3.py
@@ -3,13 +3,28 @@ from fpdf import FPDF
 
 class PDF(FPDF):
   def header(self):
-    pass
-  
-  def footer(self):
-    pass
+    self.set_font("helvetica","B",15)
+    width= self.get_string_width(self.title)+6
+    self.set_x((210-width)/2)
+    self.set_draw_color(0,80,180)
+    self.set_fill_color(230,230,0)
+    self.set_text_color(220,50,50)
+    self.set_line_width(1)
+    self.cell(width,9,self.title,new_x="LMARGIN", new_y="NEXT", align="C",fill=True)
+    self.ln()
 
-  def chapter_title(self):
-    pass
+  def footer(self):
+    self.set_y(-15)
+    self.set_font("helvetica","I",12)
+    self.set_text_color(128)
+    self.cell(0,10, f"Page {self.page_no()}",align="C")
+
+
+  def chapter_title(self, num, label):
+    self.set_font("helvetica","",12)
+    self.set_fill_color(200,220,255)
+    self.cell(0,6,f"Chapter {num} : {label}", new_x="LMARGIN",new_y="NEXT",align="L",fill=True)
+    
 
   def chapter_body(self, filepath):
 
@@ -19,16 +34,22 @@ class PDF(FPDF):
     self.multi_cell(0,5,txt)
     self.ln()
     self.set_font(style="I")
-    self.cell(0, 5, "(End of excernt)")
-    pass
+    self.cell(0,5,"(End of excerpt)")
+    
 
-  def print_chapter(self, filepath):
+  def print_chapter(self,num,title, filepath):
+    self.add_page()
+    self.chapter_title(num,title)
     self.chapter_body(filepath)
-    pass
+    
 
 pdf = PDF()
-txt=open("C:/Users/54112/Desktop/python/pythonProject/00 - Python exercises/09 - Generating PDF/para.txt","r")
+pdf.set_title("100000 Ways to learn programming")
+pdf.set_author("Juan Ruiz")
+#This creates one chapter
+pdf.print_chapter(1,"GETTING STARTED WITH PROGRAMMING","00 - Python exercises/09 - Generating PDF/para.txt")
+pdf.print_chapter(2,"WHICH PROGRAMMING LANGUAGE TO LEARN","00 - Python exercises/09 - Generating PDF/para.txt")
 
-pdf.print_chapter("C:/Users/54112/Desktop/python/pythonProject/00 - Python exercises/09 - Generating PDF/para.txt")
-#pdf.output("sample4.pdf")
-print(txt.read())
+
+
+pdf.output("00 - Python exercises/09 - Generating PDF/sample4.pdf")
