4ae2bea64f65104f97a85b223b5973247495bfc9
Juan Ruiz <jaruizdavalos@gmail.com>
2024-07-24 22:58:34 -0300

    Building PDF invoice part 2

--- 00 - Python exercises/10 - Building a PDF Invoice/01-PDFInvoice.py
+++ 00 - Python exercises/10 - Building a PDF Invoice/01-PDFInvoice.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from fpdf import FPDF
+
 
 window =Tk()
 
@@ -15,15 +17,52 @@ invoice_items=[]
 
 def add_medicine():
   select_medicine= medicine_listbox.get(ANCHOR)
-  quantity = int(quantity_entry.get())
+  if quantity_entry.get()=="":
+    quantity=1
+    quantity_entry.insert(END,1)
+  else:
+    quantity = int(quantity_entry.get())
+  
   price= medicines[select_medicine]
   item_total= price*quantity
   invoice_items.append((select_medicine,quantity,item_total))
-  print(invoice_items)
+  total_amount_entry.delete(0,END)
+  total_amount_entry.insert(END, str(calculate_total()))
+  update_invoice_text()
+
+def calculate_total():
+  total= 0.0
+  for item in invoice_items:
+    total= total+ item[2]
+  return total
+
+
+def update_invoice_text():
+  invoice_text.delete(1.0,END)
+  for item in invoice_items:
+    invoice_text.insert(END, f"Medicine: {item[0]}, Quantity: {item[1]}, Total: {item[2]}\n")
+
+def generate_invoice():
+  customer_name= customer_entry.get()
+
+  pdf= FPDF()
+  pdf.add_page()
+  pdf.set_font("Helvetica",size=12)
+
+  pdf.cell(0,10,text="Invoice", new_x="LMARGIN",new_y="NEXT",align="C")
+  pdf.cell(0,10,text="Customer: "+customer_name,new_x="LMARGIN",new_y="NEXT",align="L")
+  pdf.cell(0,10,text="",new_x="LMARGIN",new_y="NEXT")
+
+  for item in invoice_items:
+    medicine_name, quantity, item_total=item
+    pdf.cell(0,10, text=f"Medicine: {medicine_name}, Quantity: {quantity},Total: {item_total}", new_x="LMARGIN", new_y="NEXT", align="L")
+
+  pdf.cell(0,10, text="Total Amount: "+str(calculate_total()), new_x="LMARGIN",new_y="NEXT",align="L")
+
+  pdf.output("00 - Python exercises/10 - Building a PDF Invoice/invoice.pdf")
 
 
 medicine_label = Label(window, text="Medicine: ")
-
 medicine_label.pack()
 
 medicine_listbox=Listbox(window, selectmode=SINGLE)
@@ -36,6 +75,7 @@ quantity_label = Label(window, text="Quantity")
 quantity_label.pack()
 quantity_entry = Entry(window)
 quantity_entry.pack()
+
 
 add_button= Button(window, text= "Add Medicine", command=add_medicine)
 add_button.pack()
@@ -51,7 +91,7 @@ customer_label.pack()
 customer_entry=Entry(window)
 customer_entry.pack()
 
-generate_button= Button(window, text="Generate Invoice")
+generate_button= Button(window, text="Generate Invoice", command=generate_invoice)
 generate_button.pack()
 
 invoice_text = Text(window, height=10,width=50)
