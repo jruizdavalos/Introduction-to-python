01a77c8140b3d6c9a395e4aba158be009b268e9a
Juan Ruiz <jaruizdavalos@gmail.com>
2024-08-01 23:52:39 -0300

    Creating snake game part 3

--- 00 - Python exercises/12 - Creating Snake Game/01 - snakeGame.py
+++ 00 - Python exercises/12 - Creating Snake Game/01 - snakeGame.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -11,6 +12,7 @@ pygame.display.set_caption("Snake Game")
 
 white= (255,255,255)
 black=(0,0,0)
+red=(255,0,0)
 
 game_over = False
 
@@ -19,6 +21,10 @@ y1= window_height / 2
 
 x1_change= 0
 y1_change= 0
+
+foodx= round(random.randrange(0, window_witdh-10)/10)*10.0
+foody= round(random.randrange(0, window_height-10)/10)*10.0
+
 clock = pygame.time.Clock()
 
 while not game_over:
@@ -46,8 +52,15 @@ while not game_over:
   
   x1= x1 + x1_change
   y1= y1 + y1_change
+
+  if x1 >= window_witdh or x1<0 or y1>=window_height or y1<0:
+    x1=window_witdh / 2
+    y1= window_height / 2
+
+
   window.fill(black)
 
+  pygame.draw.rect(window, red, [400,300,10,10])
   pygame.draw.rect(window, white,[x1,y1,10,10])
   pygame.display.update()
   clock.tick(20)
