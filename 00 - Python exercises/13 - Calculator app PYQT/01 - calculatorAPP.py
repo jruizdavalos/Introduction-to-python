f41004d69f3f89022fe668189e5c4eb8b14de984
Juan Ruiz <jaruizdavalos@gmail.com>
2024-08-15 23:34:35 -0300

    Calculator part two

--- 00 - Python exercises/13 - Calculator app PYQT/01 - calculatorAPP.py
+++ 00 - Python exercises/13 - Calculator app PYQT/01 - calculatorAPP.py
@@ -1,6 +1,6 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QGridLayout, QHBoxLayout,QVBoxLayout,QWidget,QApplication, QLabel, QPushButton,QLineEdit,QCheckBox
-from PyQt6.QtGui import QPixmap, QFont
+from PyQt6.QtCore import Qt
 import sys
 
 class Window(QWidget):
@@ -17,17 +17,22 @@ class Window(QWidget):
     self.setLayout(layout)
 
     display = QLabel("0")
+    display.setAlignment(Qt.AlignmentFlag.AlignRight)
 
     layout.addWidget(display,0,0,1,4)
 
     buttons=[QPushButton(str(i)) for i in range(10)]
+
+    operators = ["+","-","*","/"]
+    operator_buttons = [QPushButton(op) for op in operators]
 
     for i,button in enumerate(buttons):
       row, col = divmod(i,3)
       layout.addWidget(button, row+1,col)
     
 
-
+    for i, op_button in enumerate(operator_buttons):
+      layout.addWidget(op_button,i+1,3)
 
 
 app = QApplication(sys.argv)
